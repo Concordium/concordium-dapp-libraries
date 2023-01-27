06cd3050222426a349168112179a57ce6da88e21
Michael Bisgaard Olesen <mo@concordium.com>
2023-01-27 15:00:28 +0100

    Add checks

--- packages/react-components/src/WithWalletConnector.ts
+++ packages/react-components/src/WithWalletConnector.ts
@@ -131,8 +131,8 @@ interface State {
 
 function updateMapEntry<K, V>(map: Map<K, V>, key: K | undefined, value: V | undefined) {
     const res = new Map(map);
-    if (key) {
-        if (value) {
+    if (key !== undefined) {
+        if (value !== undefined) {
             res.set(key, value);
         } else {
             res.delete(key);

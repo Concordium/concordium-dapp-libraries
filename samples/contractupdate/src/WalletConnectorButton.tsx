09f470a53fc140388cc2b180e1efce51ae002703
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2023-03-06 15:01:35 +0100

    Work

--- samples/contractupdate/src/WalletConnectorButton.tsx
+++ samples/contractupdate/src/WalletConnectorButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Button } from 'react-bootstrap';
 import {
     ConnectorType,
@@ -7,27 +7,36 @@ import {
     WalletConnectionProps,
 } from '@concordium/react-components';
 
+// TODO Stop extending (pass 'select' function instead)?
 interface Props extends WalletConnectionProps {
     connection: WalletConnection | undefined;
     connectorType: ConnectorType;
     connectorName: string;
+    disconnect: ()  => void;
+    isDisconnecting: boolean;
 }
 
 export function WalletConnectorButton(props: Props) {
-    const { connection, connectorType, connectorName } = props;
-    const { isSelected, isConnected, isDisabled, select } = useWalletConnectorSelector(
+    const { connection, connectorType, connectorName, isDisconnecting, disconnect, setActiveConnectorType } = props;
+    const { isSelected, isConnected, isDisabled } = useWalletConnectorSelector(
         connectorType,
         connection,
         props
     );
 
     const verb = isConnected ? 'Disconnect' : isSelected ? 'Using' : 'Use';
+    const handleClick = useCallback(() => {
+        if (isConnected) {
+            disconnect();
+        }
+        setActiveConnectorType(isSelected ? undefined : connectorType);
+    }, [isConnected, isSelected, setActiveConnectorType, connectorType, disconnect]);
     return (
         <Button
             className="w-100"
-            disabled={isDisabled}
+            disabled={isDisabled || isDisconnecting}
             variant={isConnected ? 'danger' : isSelected ? 'dark' : 'light'}
-            onClick={select}
+            onClick={handleClick}
         >
             {`${verb} ${connectorName}`}
         </Button>

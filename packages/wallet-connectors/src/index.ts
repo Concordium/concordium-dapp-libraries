ca0b30341b533bf2aed9e1982d89c969e20c777c
Michael Bisgaard Olesen <mo@concordium.com>
2023-03-20 13:46:35 +0100

    Update docs

--- packages/wallet-connectors/src/index.ts
+++ packages/wallet-connectors/src/index.ts
@@ -3,8 +3,8 @@ export * from './WalletConnect';
 export * from './BrowserWallet';
 
 /**
- * Project ID of Concordium's WalletConnect Cloud account for mobile wallets.
- * dApps wishing to connect to one of the official Concordium mobile wallets
- * must initialize their {@link WalletConnectConnector.create WalletConnect client} with this project ID.
+ * ID of the "Mobile Wallets" project in Concordium's WalletConnect Cloud account.
+ * dApps must initialize {@link WalletConnectConnector.create WalletConnect client} with the ID of a project within a WalletConnect Cloud account.
+ * They can use this one or one create a project in their own WalletConnect Cloud account.
  */
 export const CONCORDIUM_WALLET_CONNECT_PROJECT_ID = '76324905a70fe5c388bab46d3e0564dc';

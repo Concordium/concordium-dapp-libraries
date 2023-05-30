cc8b1d8011627aeba2e5340bc5ab8caf00f54ccc
Michael Bisgaard Olesen <mo@concordium.com>
2023-05-30 19:26:17 +0200

    Update documentation

--- packages/react-components/src/useGrpcClient.ts
+++ packages/react-components/src/useGrpcClient.ts
@@ -3,6 +3,9 @@ import { Network } from '@concordium/wallet-connectors';
 import { ConcordiumGRPCClient } from '@concordium/web-sdk';
 import { GrpcWebFetchTransport } from '@protobuf-ts/grpcweb-transport';
 
+/**
+ * React hook that obtains a gRPC Web client for interacting with a node on the appropriate network.
+ */
 export function useGrpcClient({ grpcOpts }: Network): ConcordiumGRPCClient | undefined {
     const [client, setClient] = useState<ConcordiumGRPCClient>();
     useEffect(() => {

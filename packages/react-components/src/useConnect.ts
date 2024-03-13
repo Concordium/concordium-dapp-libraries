bed064bd182eea074c0eba0e732be8edd9370897
Michael Bisgaard Olesen <mo@concordium.com>
2024-03-13 13:44:45 +0100

    Document changed 'useConnect' behavior

--- packages/react-components/src/useConnect.ts
+++ packages/react-components/src/useConnect.ts
@@ -7,7 +7,8 @@ import { errorString } from './error';
  */
 export interface Connect {
     /**
-     * Function for initiating a new connection. Any existing connection will not be automatically disconnected.
+     * Function for initiating a new connection using the provided connector or undefined if none was provided.
+     * Any existing connection will not be automatically disconnected by calling the function.
      */
     connect: (() => void) | undefined;
 
@@ -25,7 +26,7 @@ export interface Connect {
 /**
  * Hook that exposes a function for initiating a connection on the provided {@link connector} and,
  * if successful, store the resulting connection in {@link setConnection}.
- * The hook also exposes the status of the connection progress and error if initiation failed.
+ * The hook also exposes the status of the connection progress and an error if initiation failed.
  * @param connector The connector from which new connections are to be initiated.
  * @param setConnection The setter function to which new connections are passed.
  */
@@ -37,6 +38,9 @@ export function useConnect(
     const [connectError, setConnectError] = useState('');
     const connect = useMemo(() => {
         if (!connector) {
+            // There's no connector to connect to.
+            // Previously this would result in an exception being thrown by the returned function.
+            // Now we return 'undefined' instead to indicate that it isn't ready to be called.
             return undefined;
         }
         return () => {
